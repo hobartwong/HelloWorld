9f25a88fd982d39177e454d0dbeac58a5c8c849b
Hobart Wong <hobartwong@gmail.com>
2020-02-06 18:20:25 +0000

    Create Xcode project

---
+++ HelloWorld/HelloWorld.h
@@ -0,0 +1,19 @@
+//
+//  HelloWorld.h
+//  HelloWorld
+//
+//  Created by Hobart Wong on 06/02/2020.
+//  Copyright © 2020 Hobart Wong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloWorld.
+FOUNDATION_EXPORT double HelloWorldVersionNumber;
+
+//! Project version string for HelloWorld.
+FOUNDATION_EXPORT const unsigned char HelloWorldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloWorld/PublicHeader.h>
+
+
