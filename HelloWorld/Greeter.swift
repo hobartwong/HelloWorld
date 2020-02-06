1db2861f36091656fa7d55f1ca555e07d60a9c31
Hobart Wong <hobartwong@gmail.com>
2020-02-06 18:27:31 +0000

    Add Greeter class

---
+++ HelloWorld/Greeter.swift
@@ -0,0 +1,15 @@
+//
+//  Greeter.swift
+//  HelloWorld
+//
+//  Created by Hobart Wong on 06/02/2020.
+//  Copyright © 2020 Hobart Wong. All rights reserved.
+//
+
+import Foundation
+
+public class Greeter {
+	public class func greet() {
+		print("Hello World!")
+	}
+}
